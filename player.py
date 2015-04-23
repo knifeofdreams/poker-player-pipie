8b57cb9407b9ba7e45e82d1b414e40cb29258387
knifeofdreams <knifeofdreams86@gmail.com>
2015-04-23 13:59:00 +0200

    High card check

--- player.py
+++ player.py
@@ -1,10 +1,22 @@
 from random import randint
 
 class Player:
-    VERSION = "Testing snake"
+    VERSION = "High card checking snake"
 
     def betRequest(self, game_state):
-        if randint(0,1) < 0.5:
+        ONE_HIGH_CARD = game_state['players'][game_state['in_action']]['hole_cards'][0]['rank'] == 'A' or \
+                        game_state['players'][game_state['in_action']]['hole_cards'][0]['rank'] == 'Q' or \
+                        game_state['players'][game_state['in_action']]['hole_cards'][0]['rank'] == 'K' or \
+                        game_state['players'][game_state['in_action']]['hole_cards'][0]['rank'] == 'J'
+
+        OTHER_HIGH_CARD = game_state['players'][game_state['in_action']]['hole_cards'][1]['rank'] == 'A' or \
+                          game_state['players'][game_state['in_action']]['hole_cards'][1]['rank'] == 'Q' or \
+                          game_state['players'][game_state['in_action']]['hole_cards'][1]['rank'] == 'K' or \
+                          game_state['players'][game_state['in_action']]['hole_cards'][1]['rank'] == 'J'
+
+        if ONE_HIGH_CARD or OTHER_HIGH_CARD:
+            return game_state['players'][game_state['in_action']]['stack']/6
+        elif randint(0,1) < 0.5:
             return 100
         else:
             return 0
--- player_test.py
+++ player_test.py
@@ -6,11 +6,17 @@ from player import Player
 class PlayerTest(unittest.TestCase):
     def setUp(self):
         self.player = Player()
-        with open('HighCard.json') as json_file:
-            self.game_state = json.load(json_file)
 
     def test_bet_request_should_return_integer(self):
-        self.assertIsInstance(self.player.betRequest(self.game_state), int)
+        with open('HighCard.json') as json_file:
+            game_state = json.load(json_file)
+        self.assertIsInstance(self.player.betRequest(game_state), int)
+
+    def test_high_card(self):
+        with open('HighCard.json') as json_file:
+            game_state = json.load(json_file)
+
+        self.assertEqual(self.player.betRequest(game_state), 300)
 
 if __name__ == '__main__':
     unittest.main()
